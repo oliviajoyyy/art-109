0520a992c3232f29df30b2372b18ff7db2222d4d
oliviajoyyy <127996216+oliviajoyyy@users.noreply.github.com>
2024-10-06 21:02:08 -0700

    tutorial 6 p5.js custom background

--- portfolio-site-p5/js/sketch.js
+++ portfolio-site-p5/js/sketch.js
@@ -2,10 +2,13 @@
 let canvas;
 
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.position(0, 0); // same as setting css position
     canvas.style("z-index", -2); // put canvas in background
     // background(225); // no background = transparent
+
+    angleMode(DEGREES);
+    strokeWeight(2);
 }
 
 function windowResized() {
@@ -14,12 +17,37 @@ function windowResized() {
 }
 
 function draw() {
-    
+    background("#042a4a");
+    drawWave();
+}
+
+// Option 3: Geometry with Sine Waves
+function drawWave() {
+    rotateX(60);
+    noFill();
+
+    for (var i = 0; i < 50; i++) {
+        var r = map(sin(frameCount / 2), -1, 1, 100, 200);
+        var g = map(i, 0, 50, 100, 200);
+        var b = map(cos(frameCount), -1, 1, 200, 100);
+        stroke(r, g, b);
+        rotate(frameCount / 20);
+
+        beginShape()
+        for(var j = 0; j < 360; j += 90) {
+            var rad = i * 15;
+            var x = rad * cos(j);
+            var y = rad * sin(j);
+            var z = sin(frameCount * 3 + i * 10) * 100;
+            vertex(x, y, z);
+        }
+        endShape(CLOSE);
+    }
 }
 
 function mouseMoved() {
-    drawThing(mouseX, mouseY);
-    drawThing(mouseX - 50, mouseY + 75);
+    // drawThing(mouseX, mouseY);
+    // drawThing(mouseX - 50, mouseY + 75);
 }
 
 function drawThing(x, y) {
